3b6d242ac3b259ce26212555cec4e5fc44a9d03d
RS1 Project <48027364+RS1-Project@users.noreply.github.com>
2020-11-17 11:36:06 +0000

    Add option to discard transforms in useRectRef

--- src/useRectRef.js
+++ src/useRectRef.js
@@ -9,17 +9,68 @@
  * License: Apache License 2.0
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Modified on Tuesday, 17th November 2020 11:10:57 am
+ * Modified on Tuesday, 17th November 2020 11:35:38 am
  * *****************************************************************************
  */
 
 import { useState, useRef, useEffect } from 'react'
 
-export default () => {
+// Credits to Adam Leggett on StackOverflow:
+// https://stackoverflow.com/a/57876601
+const adjustedBoundingClientRect = (el, rect) => {
+    const style = window.getComputedStyle(el)
+    const tx = style.transform
+
+    if (tx) {
+        var sx, sy, dx, dy
+        if (tx.startsWith('matrix3d(')) {
+            const ta = tx.slice(9, -1).split(/, /)
+            sx = +ta[0]
+            sy = +ta[5]
+            dx = +ta[12]
+            dy = +ta[13]
+        } else if (tx.startsWith('matrix(')) {
+            const ta = tx.slice(7, -1).split(/, /)
+            sx = +ta[0]
+            sy = +ta[3]
+            dx = +ta[4]
+            dy = +ta[5]
+        } else {
+            return rect
+        }
+
+        const to = style.transformOrigin
+        const x = rect.x - dx - (1 - sx) * parseFloat(to)
+        const y =
+            rect.y - dy - (1 - sy) * parseFloat(to.slice(to.indexOf(' ') + 1))
+        const w = sx ? rect.width / sx : el.offsetWidth
+        const h = sy ? rect.height / sy : el.offsetHeight
+        return {
+            x: x,
+            y: y,
+            width: w,
+            height: h,
+            top: y,
+            right: x + w,
+            bottom: y + h,
+            left: x,
+        }
+    } else {
+        return rect
+    }
+}
+
+export default ({ withTransforms = true } = {}) => {
     const ref = useRef()
     const [rect, setRect] = useState({})
 
-    const update = () => setRect(ref?.current?.getBoundingClientRect() ?? {})
+    const update = () =>
+        setRect(() => {
+            let ret = ref?.current?.getBoundingClientRect() ?? {}
+            if (!withTransforms && ref?.current)
+                ret = adjustedBoundingClientRect(ref.current, ret)
+            return ret
+        })
 
     useEffect(() => {
         update()
